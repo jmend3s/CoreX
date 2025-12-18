77342b537ea160bd7e2f4bf57a4edac86bb9303f
Jmendes42 <jmendes.3595@gmail.com>
2025-12-18 11:33:33 +0000

    TimerService example refactor

--- zephyr_ws/corex/examples/services/timerservice/README.md
+++ zephyr_ws/corex/examples/services/timerservice/README.md
@@ -1,5 +1,5 @@
 # TimerService Example #
 
-Using software timer service to blink LED on pin 30
+Example for software timer service
 
 Be mindful that on a real application the TimerService::update() method would be called on the Application::update()
--- zephyr_ws/corex/examples/services/timerservice/main.cpp
+++ zephyr_ws/corex/examples/services/timerservice/main.cpp
@@ -1,19 +1,11 @@
 
-#include "Gpio.h"
 #include "TimerService.h"
 
 #include <zephyr/kernel.h>
 
 
-#define LED_NODE DT_ALIAS(gpioled0)
-
-static constexpr gpio_dt_spec ledSpec = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
 int main()
 {
-    Gpio led(ledSpec, Gpio::Mode::Output);
-    led.initialize();
-
     TimerService timerService;
     timerService.initialize();
 
@@ -22,7 +14,7 @@ int main()
         timerService.update();
         if (timerService.ms100())
         {
-            led.toggle();
+            printk("Tick...");
         }
     }
 }
