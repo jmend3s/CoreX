c77a4d8c7f780417dccb4fe95a0fed1ae4f6fc16
Jmendes42 <jmendes.3595@gmail.com>
2026-05-11 09:14:18 +0100

    Modifying formater, not working with vsnprint

--- zephyr_ws/corex/examples/logging/main.cpp
+++ zephyr_ws/corex/examples/logging/main.cpp
@@ -1,13 +1,27 @@
 
 #include "Delay.h"
-#include "Logger.h"
 
+#include "LoggerWorker.h"
+#include "LoggerMacros.h"
+#include "Printer.h"
+
+
+#define CK_LOGGER_MODULE "MAIN"
 
 int main()
 {
+    Printer printer;
+    LoggerWorker loggerWorker(printer);
+
+
+    CK_LOG_INFO("SYSTEM START");
+    loggerWorker.work();
     while (true)
     {
-        CK_LOG_INFO("Hello World");
-        Delay::ms(500);
+        // CK_LOG_INFO("Hello World");
+
+        // loggerWorker.work();
+
+        // Delay::ms(500);
     }
 }
