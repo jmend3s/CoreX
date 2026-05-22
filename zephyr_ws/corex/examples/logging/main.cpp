68c30468857fcc6f77278bdf461b4e381c2a4861
Jmendes42 <jmendes.3595@gmail.com>
2026-05-22 17:31:59 +0100

    Formatter almost ready

--- zephyr_ws/corex/examples/logging/main.cpp
+++ zephyr_ws/corex/examples/logging/main.cpp
@@ -15,13 +15,33 @@ int main()
 
     int32_t i = 42;
     int32_t ii = -42;
+    uint32_t ui = 42;
     float f = 4.2f;
     float ff = -4.2f;
+    auto* ptr = reinterpret_cast<void*>(0x20001000);
+    uint32_t hex = 0xDEADBEEF;
+    uint32_t binary = 0b10110110;
 
+
+    struct Binary32
+    {
+        uint32_t value;
+    };
+
+    struct Hexadecimal
+    {
+        uint32_t value;
+    };
 
     while (true)
     {
-        CK_LOG_INFO("Hello World", i, ii, f, ff);
+        CK_LOG_INFO("String: ", "Hello World!");
+        CK_LOG_INFO("int32: ", i, ii);
+        CK_LOG_INFO("uint32: ", ui);
+        CK_LOG_INFO("Float: ", f, ff);
+        CK_LOG_INFO("Pointer: ", ptr);
+        CK_LOG_INFO("Hexadecimal: ", Hexadecimal { hex });
+        CK_LOG_INFO("Binary: ", Binary32 { binary });
         loggerWorker.work();
 
         Delay::ms(500);
